db117458b02c1501e98629f0d54de51f46ab29b5
Tiniiz <ivalushka2003@gmail.com>
2022-11-04 18:41:27 +0300

    Add exception

--- client.py
+++ client.py
@@ -10,3 +10,16 @@ async def tcp_echo_client(msg):
     data = await reader.read(100)
     print(f'Получил: {data.decode()}')
     writer.close()
+
+try:
+    port = input("Введите номер порта или def для стандартного: ")
+    if port == '':
+        port = 9090
+        port = int(port)
+    if type(port) == int and 0 <= port <= 65535:
+        pass
+    else:
+        port = 9090
+except ValueError:
+    port = 9090
+    print("Введен некорректный порт. По умолчанию - 9090.")
--- server.py
+++ server.py
@@ -11,3 +11,18 @@ async def handle_echo(reader, writer):
     await writer.drain()
 
     writer.close()
+
+try:
+    port = input("Введите номер порта или def для стандартного: ")
+    if port == '':
+        port = 9090
+        port = int(port)
+    if type(port) == int and 0 <= port <= 65535:
+        pass
+    elif port == 'def':
+        port = 9090
+    else:
+        port = 9090
+except ValueError:
+    port = 9090
+    print("Введен некорректный порт. По умолчанию - 9090.")
